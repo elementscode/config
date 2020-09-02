ec0dc24f3f4e7f38565711f76f73e5221a547f3c
Chris Mather <chris@elements.dev>
2020-09-02 13:21:09 -0700

    fix find method

--- src/find.ts
+++ src/find.ts
@@ -6,17 +6,22 @@ import { Config } from './config';
  * Find the app config settings, require the file, and return the config
  * instance. If none is found then this method returns an empty config object.
  */
-export function findOrCreateAppConfig(pwd: string = process.cwd()): Config {
+export function findOrCreateAppConfig(cwd: string = process.cwd()): Config {
+  let requirePath: string;
+
   try {
-    let exports = require(path.join(pwd, 'app', 'config'));
-    if (exports instanceof Config) {
-      return exports;
-    } else if (exports.default instanceof Config) {
-      return exports.default;
-    } else {
-      return new Config();
-    }
-  } catch (err) {
+    requirePath = require.resolve(path.join(cwd, 'app', 'config'));
+  } catch(err) {
+    // this means the config file was not found.
+    return new Config();
+  }
+
+  let exports = require(requirePath);
+  if (exports instanceof Config) {
+    return exports;
+  } else if (exports.default instanceof Config) {
+    return exports.default;
+  } else {
     return new Config();
   }
 }
